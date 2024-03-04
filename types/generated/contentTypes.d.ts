006643bc59568c12b18f81c565510492321954c2
Annka-Sarviro <annka_nuha@i.ua>
2024-03-04 12:50:27 +0200

    feat: add ourOffer in patner content type

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -1071,6 +1071,12 @@ export interface ApiPartnerPartner extends Schema.CollectionType {
           localized: true;
         };
       }>;
+    ourOffer: Attribute.String &
+      Attribute.SetPluginOptions<{
+        i18n: {
+          localized: true;
+        };
+      }>;
     createdAt: Attribute.DateTime;
     updatedAt: Attribute.DateTime;
     createdBy: Attribute.Relation<
